d896045ba0a27262068e525d47fae41f32fd8fee
Dmitry <d.diuan455@gmail.com>
2024-02-21 20:54:14 +0300

    final

--- app/src/main/java/com/example/randomgame/MainActivity.kt
+++ app/src/main/java/com/example/randomgame/MainActivity.kt
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.Toast
 
 import com.example.randomgame.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
     private val handler = Handler(Looper.getMainLooper())
     private var random = 0
+    private var attempts = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,17 @@ class MainActivity : AppCompatActivity() {
         viewBinding.bt1.setOnClickListener {
             game()
         }
+
+        viewBinding.bt2.setOnClickListener {
+            random = randomNumbers()
+            invisible()
+            updateAttempts()
+
+        }
+        viewBinding.bt3.setOnClickListener {
+            stopGame()
+        }
+
     }
 
     private fun randomNumbers(): Int {
@@ -40,43 +53,65 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun stopGame() {
+        viewBinding.tvTitle.setText(R.string.thanks)
         handler.postDelayed({
             finish()
         }, 3000)
     }
 
-    private fun askAgain() {
-        viewBinding.tvTitle.setText(R.string.request)
-        when (readUserInputNumber().toString()) {
-            "да" -> {
-                viewBinding.tvTitle.setText(R.string.input_request)
-                game()
-            }
-            "нет" -> {
-                viewBinding.tvTitle.setText(R.string.thanks)
-                stopGame()
-            }
-        }
+    private fun visibility() {
+        viewBinding.bt1.visibility = View.GONE
+        viewBinding.edText.visibility = View.GONE
+        viewBinding.tvHint.visibility = View.GONE
+        viewBinding.tvHint2.visibility = View.GONE
+        viewBinding.tvQuantity.visibility = View.GONE
+        viewBinding.tvQuantity2.visibility = View.GONE
+        viewBinding.bt2.visibility = View.VISIBLE
+        viewBinding.bt3.visibility =View.VISIBLE
+    }
+
+    private fun invisible(){
+        viewBinding.bt1.visibility = View.VISIBLE
+        viewBinding.edText.visibility = View.VISIBLE
+        viewBinding.tvHint.visibility = View.VISIBLE
+        viewBinding.tvHint2.visibility = View.VISIBLE
+        viewBinding.tvQuantity.visibility = View.VISIBLE
+        viewBinding.tvQuantity2.visibility = View.VISIBLE
+        viewBinding.edText.visibility = View.VISIBLE
+        viewBinding.bt2.visibility = View.GONE
+        viewBinding.bt3.visibility = View.GONE
+        viewBinding.tvTitle.setText(R.string.input_request)
+        attempts = 0
+
+    }
+
+    private fun updateAttempts(){
+        viewBinding.tvQuantity2.text = getString(R.string.attempts_format).format(attempts)
     }
 
     private fun game() {
         val userInput = readUserInputNumber()
+        attempts++
+        updateAttempts()
         if (userInput != null) {
             if (userInput == random) {
                 viewBinding.tvHint2.text = null
-                viewBinding.tvTitle.setText(R.string.offset)
-                askAgain()
-
+                viewBinding.edText.text = null
+                viewBinding.tvTitle.setText(R.string.request)
+                visibility()
             } else {
                 if (userInput > random) {
-                    viewBinding.tvHint2.setText(R.string.hint1)
+                    viewBinding.edText.text = null
+                    viewBinding.tvHint2.text = getString(R.string.hint1,).format(userInput)
                 } else {
-                    viewBinding.tvHint2.setText(R.string.hint2)
+                    viewBinding.edText.text = null
+                    viewBinding.tvHint2.text = getString(R.string.hint2).format(userInput)
                 }
             }
         } else {
              Toast.makeText(this, "Введите корректное число", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
 
