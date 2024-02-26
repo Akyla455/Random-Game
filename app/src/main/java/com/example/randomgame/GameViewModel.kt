b9369d65ba5a6e504f485c46b976805af84e2f6e
Dmitry <d.diuan455@gmail.com>
2024-02-26 19:22:02 +0300

    final

--- app/src/main/java/com/example/randomgame/GameViewModel.kt
+++ app/src/main/java/com/example/randomgame/GameViewModel.kt
@@ -1,4 +1,60 @@
 package com.example.randomgame
 
-class GameViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+sealed class GameState{
+    object InputRequest: GameState()
+    object Win: GameState()
+    object Greater: GameState()
+    object Less: GameState()
+
+
+
+
+}
+class GameViewModel: ViewModel() {
+
+    private var random = randomNumbers()
+    var attempts = 0
+
+    private val _gameState = MutableLiveData<GameState>()
+    val gameState: LiveData<GameState>
+        get() = _gameState
+
+    init {
+        startNewGame()
+    }
+    private fun startNewGame(){
+        random = randomNumbers()
+        attempts = 0
+        _gameState.value = GameState.InputRequest
+    }
+
+    fun checkUserInput(userInput: Int){
+        attempts++
+        if(userInput == random){
+            _gameState.value = GameState.Win
+        } else if (userInput > random){
+            _gameState.value = GameState.Greater
+
+        } else _gameState.value = GameState.Less
+
+
+    }
+
+    fun restartTheGame(){
+       startNewGame()
+    }
+
+    private fun randomNumbers(): Int {
+        val min = 1
+        val max = 10
+        return (min..max).random()
+    }
+
+
+
 }
--- app/src/main/java/com/example/randomgame/MainActivity.kt
+++ app/src/main/java/com/example/randomgame/MainActivity.kt
@@ -1,118 +1,114 @@
 package com.example.randomgame
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 import android.widget.Toast
-
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.randomgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
-    private val handler = Handler(Looper.getMainLooper())
-    private var random = 0
-    private var attempts = 0
-
+    private val viewModel: GameViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
 
-
-        random = randomNumbers()
-
-        viewBinding.bt1.setOnClickListener {
-            game()
-        }
+        setupViews()
+        observerGameState()
 
         viewBinding.bt2.setOnClickListener {
-            random = randomNumbers()
-            invisible()
+            viewModel.restartTheGame()
             updateAttempts()
+            invisible()
 
         }
+
         viewBinding.bt3.setOnClickListener {
-            stopGame()
+            finish()
         }
 
-    }
 
-    private fun randomNumbers(): Int {
-        val min = 1
-        val max = 10
-        return (min..max).random()
-    }
 
-    private fun readUserInputNumber(): Int? {
-        val userNumber = viewBinding.edText.text.toString()
-        return userNumber.toIntOrNull()
+
     }
 
 
 
-    private fun stopGame() {
-        viewBinding.tvTitle.setText(R.string.thanks)
-        handler.postDelayed({
-            finish()
-        }, 3000)
+
+    private fun setupViews(){
+        viewBinding.tvQuantity2.text = viewModel.attempts.toString()
+        viewBinding.bt1.setOnClickListener {
+            val userInput = viewBinding.edText.text.toString().toIntOrNull()
+            if(userInput != null){
+                viewModel.checkUserInput(userInput)
+                updateAttempts()
+            } else showToast("Введите корректное значение")
+        }
     }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun updateAttempts(){
+        viewBinding.tvQuantity2.text = viewModel.attempts.toString()
+
+    }
+
+
+    fun observerGameState(){
+        viewModel.gameState.observe(this, Observer { gameState ->
+            when(gameState){
+                is GameState.InputRequest -> {
+                    viewBinding.tvTitle.setText(R.string.input_request)
+                    viewBinding.edText.text = null
+                }
+                is GameState.Win -> {
+                    viewBinding.tvTitle.setText(R.string.request)
+                    viewBinding.edText.text = null
+                    viewBinding.tvHint2.text = null
+                    visibility()
+                }
+                is GameState.Greater ->{
+                    viewBinding.tvHint2.setText(R.string.hint1)
+                    viewBinding.edText.text = null
+                }
+                is GameState.Less -> {
+                    viewBinding.tvHint2.setText(R.string.hint2)
+                    viewBinding.edText.text = null
+                }
+
+            }
+
+        })
+    }
+
+
+
+
+
+
+
+
 
     private fun visibility() {
-        viewBinding.bt1.visibility = View.GONE
-        viewBinding.edText.visibility = View.GONE
-        viewBinding.tvHint.visibility = View.GONE
-        viewBinding.tvHint2.visibility = View.GONE
-        viewBinding.tvQuantity.visibility = View.GONE
-        viewBinding.tvQuantity2.visibility = View.GONE
-        viewBinding.bt2.visibility = View.VISIBLE
-        viewBinding.bt3.visibility =View.VISIBLE
+        viewBinding.group1.visibility = View.GONE
+        viewBinding.group2.visibility = View.VISIBLE
     }
 
     private fun invisible(){
-        viewBinding.bt1.visibility = View.VISIBLE
-        viewBinding.edText.visibility = View.VISIBLE
-        viewBinding.tvHint.visibility = View.VISIBLE
-        viewBinding.tvHint2.visibility = View.VISIBLE
-        viewBinding.tvQuantity.visibility = View.VISIBLE
-        viewBinding.tvQuantity2.visibility = View.VISIBLE
-        viewBinding.edText.visibility = View.VISIBLE
-        viewBinding.bt2.visibility = View.GONE
-        viewBinding.bt3.visibility = View.GONE
-        viewBinding.tvTitle.setText(R.string.input_request)
-        attempts = 0
+        viewBinding.group1.visibility = View.VISIBLE
+        viewBinding.group2.visibility = View.GONE
+        //viewBinding.tvTitle.setText(R.string.input_request)
+
 
     }
 
-    private fun updateAttempts(){
-        viewBinding.tvQuantity2.text = getString(R.string.attempts_format).format(attempts)
-    }
 
-    private fun game() {
-        val userInput = readUserInputNumber()
-        attempts++
-        updateAttempts()
-        if (userInput != null) {
-            if (userInput == random) {
-                viewBinding.tvHint2.text = null
-                viewBinding.edText.text = null
-                viewBinding.tvTitle.setText(R.string.request)
-                visibility()
-            } else {
-                if (userInput > random) {
-                    viewBinding.edText.text = null
-                    viewBinding.tvHint2.text = getString(R.string.hint1,).format(userInput)
-                } else {
-                    viewBinding.edText.text = null
-                    viewBinding.tvHint2.text = getString(R.string.hint2).format(userInput)
-                }
-            }
-        } else {
-             Toast.makeText(this, "Введите корректное число", Toast.LENGTH_SHORT).show()
-        }
 
-    }
 }
 
 
